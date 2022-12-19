3447fc49d0f79beae98191ed8092db5bac6cd41f
MorningPants <77476821+MorningPants@users.noreply.github.com>
2022-12-19 00:35:44 -0800

    Add feature detail page

--- controllers/featuresController.js
+++ controllers/featuresController.js
@@ -1,4 +1,6 @@
 const Feature = require("../models/feature");
+const Animal = require("../models/animal");
+const async = require("async");
 
 // Display list of all Features.
 exports.feature_list = function (req, res, next) {
@@ -13,9 +15,36 @@ exports.feature_list = function (req, res, next) {
   };
 
 // Display detail page for a specific Feature.
-exports.feature_detail = (req, res) => {
-  res.send(`NOT IMPLEMENTED: Feature detail: ${req.params.id}`);
-};
+exports.feature_detail = (req, res, next) => {
+    async.parallel(
+      {
+        feature(callback) {
+          Feature.findById(req.params.id).exec(callback);
+        },
+  
+        feature_animals(callback) {
+          Animal.find({ feature: req.params.id }).exec(callback);
+        },
+      },
+      (err, results) => {
+        if (err) {
+          return next(err);
+        }
+        if (results.feature == null) {
+          // No results.
+          const err = new Error("Feature not found");
+          err.status = 404;
+          return next(err);
+        }
+        // Successful, so render
+        res.render("feature_detail", {
+          title: "Feature Detail",
+          feature: results.feature,
+          feature_animals: results.feature_animals,
+        });
+      }
+    );
+  };
 
 // Display Feature create form on GET.
 exports.feature_create_get = (req, res) => {
